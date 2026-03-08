5bac2a06ca415f093294b41c25b67efe6cd74fd8
Klodvik <denis.kostoglodov.04@gmail.com>
2026-03-09 02:19:48 +0700

    Исправлено наименование методов

--- src/main/java/io/github/Klodvik1/mapper/UserMapper.java
+++ src/main/java/io/github/Klodvik1/mapper/UserMapper.java
@@ -5,7 +5,7 @@ import io.github.Klodvik1.dto.UserResponseDto;
 import io.github.Klodvik1.entity.User;
 
 public class UserMapper {
-    public User toEntity(UserRequestDto userRequestDto) {
+    public User toUser(UserRequestDto userRequestDto) {
         User user = new User();
         user.setName(userRequestDto.name());
         user.setEmail(userRequestDto.email());
@@ -14,7 +14,7 @@ public class UserMapper {
         return user;
     }
 
-    public UserResponseDto toResponseDto(User user) {
+    public UserResponseDto toUserResponseDto(User user) {
         return new UserResponseDto(
                 user.getId(),
                 user.getName(),
