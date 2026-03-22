449a17e43d86400d1dd9d3f2070fc6267076a1ea
Klodvik <denis.kostoglodov.04@gmail.com>
2026-03-22 19:33:36 +0700

    Изменён маппинг пользователя

--- src/main/java/io/github/Klodvik1/mapper/UserMapper.java
+++ src/main/java/io/github/Klodvik1/mapper/UserMapper.java
@@ -3,15 +3,21 @@ package io.github.Klodvik1.mapper;
 import io.github.Klodvik1.dto.UserRequestDto;
 import io.github.Klodvik1.dto.UserResponseDto;
 import io.github.Klodvik1.entity.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserMapper {
     public User toUser(UserRequestDto userRequestDto) {
         User user = new User();
+        applyRequestDto(user, userRequestDto);
+
+        return user;
+    }
+
+    public void applyRequestDto(User user, UserRequestDto userRequestDto) {
         user.setName(userRequestDto.name());
         user.setEmail(userRequestDto.email());
         user.setAge(userRequestDto.age());
-
-        return user;
     }
 
     public UserResponseDto toUserResponseDto(User user) {
