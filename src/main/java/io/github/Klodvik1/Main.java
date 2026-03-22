93c044782ea619f5887043e7d956a72667896fb6
Klodvik <denis.kostoglodov.04@gmail.com>
2026-03-22 19:29:59 +0700

    Добавлено подключение Spring Boot и Spring Data JPA

--- src/main/java/io/github/Klodvik1/Main.java
+++ src/main/java/io/github/Klodvik1/Main.java
@@ -1,34 +1,11 @@
 package io.github.Klodvik1;
 
-import io.github.Klodvik1.config.HibernateSessionFactoryProvider;
-import io.github.Klodvik1.dao.UserDao;
-import io.github.Klodvik1.dao.UserDaoImpl;
-import io.github.Klodvik1.mapper.UserMapper;
-import io.github.Klodvik1.service.UserService;
-import io.github.Klodvik1.service.UserServiceImpl;
-import io.github.Klodvik1.ui.UserConsoleMenu;
-import io.github.Klodvik1.validation.UserValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
-        LOGGER.info("UserService запущен.");
-
-        UserDao userDao = new UserDaoImpl();
-        UserMapper userMapper = new UserMapper();
-        UserValidator userValidator = new UserValidator();
-        UserService userService = new UserServiceImpl(userDao, userMapper, userValidator);
-        UserConsoleMenu userConsoleMenu = new UserConsoleMenu(userService);
-
-        try {
-            userConsoleMenu.start();
-        }
-        finally {
-            HibernateSessionFactoryProvider.shutdown();
-            LOGGER.info("Работа UserService завершена.");
-        }
+        SpringApplication.run(Main.class, args);
     }
 }
