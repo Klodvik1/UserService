785917d335eefc9f537274799911159632134543
Klodvik <denis.kostoglodov.04@gmail.com>
2026-03-22 19:35:48 +0700

    Удалён консольный интерфейс, старая реализация Hibernate и старые тесты

--- src/main/java/io/github/Klodvik1/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package io.github.Klodvik1.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
